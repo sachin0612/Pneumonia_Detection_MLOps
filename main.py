9da2734848ea6128e7042dc9b7cfcdee73c494bb
sachin0612 <Sachinsoni600517@gmail.com>
2023-09-16 22:19:31 +0530

    prepare base model

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from src.logger import logging
 from src.exception import CustomException
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from src.pipeline.stage_03_training import ModelTrainingPipeline
 # from src.pipeline.stage_04_evaluation import EvaluationPipeline
 
@@ -15,3 +15,13 @@ try:
    logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
    raise CustomException(e,sys)
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logging.info(f"*******************")
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        raise CustomException(e,sys)
--- src/pipeline/stage_02_prepare_base_model.py
+++ src/pipeline/stage_02_prepare_base_model.py
@@ -1,29 +1,31 @@
 from src.config.configuration import ConfigurationManager
-from src.components.data_ingestion import DataIngestion
+from src.components.prepare_base_model import PrepareBaseModel
 from src.logger import logging
 from src.exception import CustomException
 import sys
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Prepare base model"
 
-class DataIngestionTrainingPipeline:
+class PrepareBaseModelTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
 
 
 
 
 if __name__ == '__main__':
     try:
+        logging.info(f"*******************")
         logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
