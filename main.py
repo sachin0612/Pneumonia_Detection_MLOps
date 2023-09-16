6f60881f144422d67dc24f09530d9f4d7e029a4f
sachin0612 <Sachinsoni600517@gmail.com>
2023-09-16 13:59:06 +0530

    data_ingestion stage completed

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from src.logger import logging
+from src.exception import CustomException
+from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from src.pipeline.stage_03_training import ModelTrainingPipeline
+# from src.pipeline.stage_04_evaluation import EvaluationPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+   raise CustomException(e,sys)
